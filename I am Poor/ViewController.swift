ddb0f43d5bada52db33a26504d741cd6fe2f08b4
coderBri <coderbri23@gmail.com>
2024-09-20 02:20:28 -0400

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Brittney Perez on 9/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
